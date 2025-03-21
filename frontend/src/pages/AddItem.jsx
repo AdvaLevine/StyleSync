98e5515573e506b8050ebdb248aac808f4a9087b
Adva Rose Levine <121626484+AdvaLevine@users.noreply.github.com>
2025-03-21 18:26:51 +0200

    home screen buttons links adjsted the login screen components to look nicer and added the resultion w media queries

---
+++ frontend/src/pages/AddItem.jsx
@@ -0,0 +1,7 @@
+const AddItem = () => {
+    return (  
+        <h1>Thi is the Add item!</h1>
+    );
+}
+ 
+export default AddItem;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "../assets/styles/Login.css";
-import logo from "../assets/images/Style_Sync_Logo.png";
+import "../assets/styles/login.css";
+import logo from "../assets/images/Logo.png";
 import { Link } from "react-router-dom";
 
 const Login = () => {
@@ -15,24 +15,23 @@ const Login = () => {
       <div className="login-box">
         {/* Login Title */}
         <h2>Login</h2>
-        <p className="subtitle">Sign in to continue</p>
+        <p className="login-subtitle">Sign in to continue</p>
 
         {/* Login Form */}
         <form>
-          <label>Name</label>
+          <label >Name</label>
           <input type="text" placeholder="Enter your name" required />
 
-          <label>Password</label>
+          <label >Password</label>
           <input type="password" placeholder="********" required />
 
-          <button type="submit">Log in</button>
+          <button type="submit">Sign in</button>
         </form>
 
         {/* Forgot Password & Signup Links */}
         <div className="links">
           <a href="https://www.google.com/">Forgot Password?</a>
-          <div></div>
-          <Link to="/signup">Signup!</Link>
+          <Link to="/signup">Sign up!</Link>
         </div>
       </div>
     </div>
