0d7b00544dde690712d9ade3f0db044bbf5877d6
Adva Rose Levine <121626484+AdvaLevine@users.noreply.github.com>
2025-04-09 19:10:19 +0300

    fixed in signup the errors and exceptions , changed the signup background

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -50,42 +50,47 @@ const Signup = () => {
     <div className="signup-container">
       {/* Back Button */}
       <Link to="/" className="back-button">⟵</Link>
-
+      <video
+          autoPlay
+          muted
+          loop
+          className="background-video"
+        >
+          <source src={require('../assets/videos/SignUpBackground.mp4')} type="video/mp4" />
+            Your browser does not support the video tag.
+        </video>
       {/* Signup Box */}
       <div className="signup-box">
         <h2>Create Account</h2>
         <p className="subtitle subtitle-top">Sign up to get started!</p>
         <form  className="signup-form" onSubmit={handleSubmit}>
           <label>Name</label>
-          <input className="signup-input" type="text" 
-           placeholder={name ? "" : "Enter your name"} 
-          required 
-          onFocus={() => setName(" ")} 
-          onBlur={(e) => !e.target.value.trim() && setName("")}
-          onChange={(e) => setName(e.target.value)}
-          value={name}
+          <input 
+            className="signup-input" 
+            type="text" 
+            placeholder="Enter your name" 
+            required 
+            onChange={(e) => setName(e.target.value)}
+            value={name}
           />
-
           <label>Email</label>
-          <input className="signup-input" type="email" 
-          placeholder={email ? "" : "Enter your email"}
-          required
-          onFocus={() => setEmail(" ")} 
-          onBlur={(e) => !e.target.value.trim() && setEmail("")} 
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
+          <input 
+            className="signup-input" 
+            type="email" 
+            placeholder="Enter your email"
+            required 
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
-
           <label>Password</label>
-          <input className="signup-input" type="password" 
-          placeholder={password ? "" : "********"}
-          required
-          onFocus={() => setPassword(" ")} // Temporarily set a space to hide placeholder
-          onBlur={(e) => !e.target.value.trim() && setPassword("")} // Restore if empty
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
+          <input 
+            className="signup-input" 
+            type="password" 
+            placeholder="********"
+            required 
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
           />
-
           <label>Date of Birth</label>
           <input className="signup-input" type="date" min="1910-01-01" max="2009-12-31" required value={dob} onChange={(e) => setDob(e.target.value)}/>
 
