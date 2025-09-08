9650e7a2ca3e95e5ffa4f092537e36653f6fe0c2
Adva Rose Levine <121626484+AdvaLevine@users.noreply.github.com>
2025-09-09 01:20:41 +0300

    Add useLocation import to App component for scroll restoration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import AddItem from "./pages/AddItem";
