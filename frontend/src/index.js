af903d35aee0305de7825a847ede96ede44986c9
TalDorS <taldor1999@gmail.com>
2025-09-08 22:15:49 +0300

    back to dev mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,10 +8,10 @@ import { AuthProvider } from 'react-oidc-context';
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_LvYLNwjnh",
   client_id: "6jt8p3s82dcj78eomqpra1qo0i",
-  redirect_uri: "https://main.d1qreohr4migr5.amplifyapp.com/login",
-  //redirect_uri: "http://localhost:3000/login",
-  post_logout_redirect_uri: "https://main.d1qreohr4migr5.amplifyapp.com/login",
-  //post_logout_redirect_uri: "https://us-east-1lvylnwjnh.auth.us-east-1.amazoncognito.com/login?client_id=6jt8p3s82dcj78eomqpra1qo0i&response_type=code&scope=email+openid&redirect_uri=http://localhost:3000/login",
+  //redirect_uri: "https://main.d1qreohr4migr5.amplifyapp.com/login",
+  redirect_uri: "http://localhost:3000/login",
+  //post_logout_redirect_uri: "https://main.d1qreohr4migr5.amplifyapp.com/login",
+  post_logout_redirect_uri: "https://us-east-1lvylnwjnh.auth.us-east-1.amazoncognito.com/login?client_id=6jt8p3s82dcj78eomqpra1qo0i&response_type=code&scope=email+openid&redirect_uri=http://localhost:3000/login",
   response_type: "code",
   scope: "email openid profile",
 };
