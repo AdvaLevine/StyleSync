976def6ddc99239e9b24b9ffe6969c62639594bf
Adva Rose Levine <121626484+AdvaLevine@users.noreply.github.com>
2025-09-13 01:02:30 +0300

    Update Cognito authority URL for authentication configuration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import { AuthProvider } from 'react-oidc-context';
 //https://main.d42bas3xeg2w1.amplifyapp.com/login (new)
 // Dev Cognito - http://localhost:3000/login
 const cognitoAuthConfig = {
-  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_LvYLNwjnh",
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_V2oTCr6Jg",
   client_id: "2k4ish6qqks2nq1ghk48nc81td",
   redirect_uri: "https://main.d42bas3xeg2w1.amplifyapp.com/login",
   //redirect_uri: "http://localhost:3000/login",
