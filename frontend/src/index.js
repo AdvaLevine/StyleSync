69404ef6f5d938591501b5ca596445d5f8156cd9
Adva Rose Levine <121626484+AdvaLevine@users.noreply.github.com>
2025-09-13 00:41:49 +0300

    revert

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from 'react-oidc-context';
 
-// Current production configuration
+// Production Cognito - https://main.d1qreohr4migr5.amplifyapp.com/login
+// Dev Cognito - http://localhost:3000/login
 const cognitoAuthConfig = {
-    authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_V2oTCr6Jg",
-    client_id: "2k4ish6qqks2nq1ghk48nc81td",
-    redirect_uri: "https://main.d42bas3xeg2w1.amplifyapp.com/login",
-    post_logout_redirect_uri: "https://main.d42bas3xeg2w1.amplifyapp.com/login",
-    response_type: "code",
-    scope: "email openid profile",
-    onSigninCallback: () => {
-        // Clean up the URL after login
-        window.history.replaceState({}, document.title, window.location.pathname);
-    }
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_LvYLNwjnh",
+  client_id: "6jt8p3s82dcj78eomqpra1qo0i",
+  redirect_uri: "https://main.d1qreohr4migr5.amplifyapp.com/login",
+  //redirect_uri: "http://localhost:3000/login",
+  post_logout_redirect_uri: "https://main.d1qreohr4migr5.amplifyapp.com/login",
+  //post_logout_redirect_uri: "https://us-east-1lvylnwjnh.auth.us-east-1.amazoncognito.com/login?client_id=6jt8p3s82dcj78eomqpra1qo0i&response_type=code&scope=email+openid&redirect_uri=http://localhost:3000/login",
+  response_type: "code",
+  scope: "email openid profile",
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
